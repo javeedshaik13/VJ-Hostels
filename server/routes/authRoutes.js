24ec7132133e6a5e0a7d226dd37aac65ebe80cf0
Tarana Khanna <taranakhanna5@gmail.com>
2025-08-19 13:48:36 +0530

    Google OAuth setup in backend

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const passport = require('passport');
+
+const router = express.Router();
+
+router.get('/google', 
+    passport.authenticate('google', { scope: ['profile', 'email']})
+);
+
+router.get('/google/callback', 
+    passport.authenticate('google', { failureRedirect: '/' }),
+        (req, res) => {
+            res.redirect('http://localhost:5173/login'); // Redirect to your dashboard or home page after successful login
+        }
+);
+
+router.get('/logout', (req, res) => {
+    req.logout(err => {
+        if(err) return next(err);
+        res.redirect('/');
+    })
+})
+
+module.exports = router;
